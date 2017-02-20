f072286a29cb34dc49f28ca518c37736e5f08ced
boungkwan <casiru.dev@gmail.com>
2017-02-20 22:53:58 +0900

    add oggle

--- apps/services/content.js
+++ apps/services/content.js
@@ -2,6 +2,13 @@ const request = require('request');
 const conDao = require('../dao/contents');
 const celabDao = require('../dao/celab');
 
+const ogglePrefix = [
+    '오빠~ 밥 먹었어? 내 생각날때 한번 봐봐~\n',
+    '오빠 나 뭐하는지 궁금하지? 이거 봐라\n',
+    '나 오빠 보고싶엉, 오빠도 나 보고싶지?\n',
+    '오빠 바쁘더라도 너무 무리하지마~. 이거 보고 힘내용\n'
+];
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
@@ -13,8 +20,9 @@ function getCelebContent(type, celebId) {
         }
         let index = getRandomInt(rows.length - 1);
         if (rows && rows.length > 0) {
-            return rows[index].url;
+            return ogglePrefix[getRandomInt(ogglePrefix.length - 1)] + rows[index].url;
         }
+        return null;
     });
 }
 
@@ -57,7 +65,7 @@ function getMoreContentsAboutCeleb(celebName) {
                 return null;
             }
             let index = getRandomInt(rows.length - 1);
-            return rows[index].url;
+            return ogglePrefix[getRandomInt(ogglePrefix.length - 1)] + rows[index].url;
         });
     });
 }
